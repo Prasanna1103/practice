efebdd84a71c913277665a209752914ad77a6c06
PRASANNA <prasnnaramesh03@gmail.com>
2024-11-05 19:24:52 +0530

    Finished rreverse int

--- python/reverse_int.py
+++ python/reverse_int.py
@@ -1,13 +1,13 @@
 import math
 
 def reverse(num):
-    a = num%10
-    print("a = ", a)
-    b = math.floor((num / 10)%10)
-    print("b = ", b)
-    c= math.floor((num/100)%10)
-    print("c = ", c)
-    return a*100 + b*10 + c
+    result = 0
+    while num > 0:
+        a = num%10
+        result = result*10+a
+        num = math.floor(num / 10)
+        print(f"a = {a}, num = {result}")
+    return result
 
 num = 7234
 print(f"original number: {num}")
